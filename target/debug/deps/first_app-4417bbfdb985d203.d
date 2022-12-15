823434b8e556e18bfe239514cef877cbc1c30879
Benjamin Aho <Benjamin.aho27@gmail.com>
2022-12-14 20:34:32 -0500

    rust files added

---
+++ target/debug/deps/first_app-4417bbfdb985d203.d
@@ -0,0 +1,5 @@
+/Users/benjaminaho/Documents/rust/first_app/target/debug/deps/first_app-4417bbfdb985d203.rmeta: src/main.rs
+
+/Users/benjaminaho/Documents/rust/first_app/target/debug/deps/first_app-4417bbfdb985d203.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/first_app-d408bad8040d8737.d
@@ -0,0 +1,5 @@
+/Users/benjaminaho/Documents/rust/first_app/target/debug/deps/first_app-d408bad8040d8737.rmeta: src/main.rs
+
+/Users/benjaminaho/Documents/rust/first_app/target/debug/deps/first_app-d408bad8040d8737.d: src/main.rs
+
+src/main.rs:
